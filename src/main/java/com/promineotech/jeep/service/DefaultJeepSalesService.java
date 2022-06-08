1f28860f2f4e3025f44ce0589ac08f12c014b8cd
Harvis DeLeon <71193615+harvisd@users.noreply.github.com>
2022-06-08 04:53:56 -0400

    Add files via upload

---
+++ src/main/java/com/promineotech/jeep/service/DefaultJeepSalesService.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.promineotech.jeep.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.promineotech.jeep.entity.Jeep;
+import com.promineotech.jeep.entity.JeepModel;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author harvi
+ *
+ */
+@Service
+@Slf4j
+public class DefaultJeepSalesService implements JeepSalesService {
+
+
+  public List<Jeep> fetchJeeps(JeepModel model, String trim){
+    log.info("The service was called for model={} and the trim={}", model, trim);
+    return null;
+  }
+  
+  
+}
